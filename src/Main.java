1308c0440ed4a6344b4f5f2764a1682851086e89
Martin Storgaard <20140000@post.au.dk>
2015-07-31 10:48:50 +0200

    Only prints greeting

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Greet g = new Greet();
         String greeting = g.greet("world");
-        System.out.println("greeting = " + greeting);
+        System.out.println(greeting);
     }
 }
