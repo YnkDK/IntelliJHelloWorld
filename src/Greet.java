9bcb452639081147f24bd45d3fa33c616cadf883
Martin Storgaard <20140000@post.au.dk>
2015-07-31 10:45:56 +0200

    initial commit

---
+++ src/Greet.java
@@ -0,0 +1,8 @@
+/**
+ * Created by mys on 7/31/15.
+ */
+public class Greet {
+    public String greet(String target) {
+        return "Hello " + target + "!";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Greet g = new Greet();
+        String greeting = g.greet("world");
+        System.out.println("greeting = " + greeting);
+    }
+}
